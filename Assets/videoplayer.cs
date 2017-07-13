da7bb8a1c0080c1009c322cd7afcfa8622a69c1e
Fiachra Matthews <fiachra@galvanic.ie>
2017-07-13 09:30:41 +0100

    First Pass Cinema Scene

---
+++ Assets/videoplayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class videoplayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MovieTexture movie = GetComponent<Renderer>().material.mainTexture as MovieTexture;
+		movie.Play();
+		movie.loop = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
